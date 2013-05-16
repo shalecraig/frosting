6a53ff83239b6e928099bce769a1f529371af4e0
Martin Poirier Theoret <mpoiriert@gmail.com>
2013-05-16 12:54:51 -0400

    [b] infinit loop file loading was possible

--- src/Frosting/Framework/ConfigurationFileLoader.php
+++ src/Frosting/Framework/ConfigurationFileLoader.php
@@ -14,17 +14,35 @@ namespace Frosting\Framework;
  */
 class ConfigurationFileLoader 
 {
+  private $loadedFiles;
   public function load($filename)
   {
+    $this->loadedFiles = array();
+    $result = $this->loadFile($filename);
+    $this->loadedFiles = array();
+    return $result;
+  }
+  
+  private function loadFile($filename)
+  {
     if(!is_array($filename)) {
+      $filename = realpath($filename);
       if(!file_exists($filename)) {
         return null;
       }
+      
+      //This is to prevent infinite loop of including files
+      if(in_array($filename, $this->loadedFiles)) {
+        return array();
+      }
+      
+      $this->loadedFiles[] = $filename;
       ob_start();
       include($filename);
       $content = ob_get_clean();
       $result = json_decode($content,true);
       $basePath = dirname($filename);
+      
     } else {
       $basePath = null;
       $result = $filename;
@@ -44,10 +62,10 @@ class ConfigurationFileLoader
     foreach($files as $file) {
       switch(true) {
         case file_exists($file):
-          $result[] = $this->load($file);
+          $result[] = $this->loadFile($file);
           break;
         case !is_null($basePath) && file_exists($basePath . DIRECTORY_SEPARATOR . $file):
-          $result[] = $this->load($basePath . DIRECTORY_SEPARATOR . $file);
+          $result[] = $this->loadFile($basePath . DIRECTORY_SEPARATOR . $file);
           break;
       }
     }
